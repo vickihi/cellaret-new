a904689464115f094b1651ebc5e17367814dbcd5
Weiwei Guo <vickihi2000@gmail.com>
2026-03-23 15:20:21 -0400

    add product model

---
+++ products/services/saq_api.py
@@ -0,0 +1,15 @@
+import requests
+
+URL = "https://catalog-service.adobe.io/graphql"
+
+HEADERS = {
+    "Content-Type": "application/json",
+    "x-api-key": "7a7d7422bd784f2481a047e03a73feaf",
+    "magento-customer-group": "b6589fc6ab0dc82cf12099d1c2d40ab994e8410c",
+    "magento-environment-id": "2ce24571-9db9-4786-84a9-5f129257ccbb",
+    "magento-store-code": "main_website_store",
+    "magento-store-view-code": "fr",   
+    "magento-website-code": "base"
+}
+
+
