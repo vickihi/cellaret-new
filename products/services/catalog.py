b7f03a61de2977115162bf1e216800f28d20c797
lzooeun <elinzoo0919@gmail.com>
2026-03-27 19:10:21 -0400

    Add control bar and dynamic pagination components

--- products/services/catalog.py
+++ products/services/catalog.py
@@ -12,9 +12,10 @@ class CatalogPageData:
     page_obj: object
     products: object
     search_query: str
+    page_range: list
 
 
-def build_catalog_page(*, data, page_number, per_page: int = 20) -> CatalogPageData:
+def build_catalog_page(*, data, page_number, per_page: int = 24) -> CatalogPageData:
     form = ProductCatalogSearchForm(data)
     is_valid = form.is_valid()
 
@@ -22,10 +23,12 @@ def build_catalog_page(*, data, page_number, per_page: int = 20) -> CatalogPageD
     products_qs = get_catalog_products(search_query=search_query)
     paginator = Paginator(products_qs, per_page)
     page_obj = paginator.get_page(page_number)
+    elided_page_range = paginator.get_elided_page_range(page_obj.number, on_each_side=1, on_ends=1) # type: ignore
 
     return CatalogPageData(
         form=form,
         page_obj=page_obj,
         products=page_obj.object_list,
         search_query=search_query,
+        page_range=elided_page_range,
     )
--- products/views.py
+++ products/views.py
@@ -4,9 +4,17 @@ from .services import build_catalog_page
 
 
 def product_catalog(request):
+    per_page_val = request.GET.get("per_page", 24)
+
+    try:
+        per_page_val = int(per_page_val)
+    except ValueError:
+        per_page_val = 24
+
     catalog_page = build_catalog_page(
         data=request.GET or None,
         page_number=request.GET.get("page"),
+        per_page=per_page_val,
     )
 
     return render(
@@ -17,5 +25,6 @@ def product_catalog(request):
             "page_obj": catalog_page.page_obj,
             "products": catalog_page.products,
             "search_query": catalog_page.search_query,
+            "page_range": catalog_page.page_range,
         },
     )
