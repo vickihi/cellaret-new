a5d6b3a1eb231623157c979eeed713337e4c5727
XiaH-dev <6367321@edu.vaniercollege.qc.ca>
2026-03-31 16:12:13 -0400

    change the order of the sort options: name before price

--- products/forms.py
+++ products/forms.py
@@ -13,10 +13,10 @@ class ProductCatalogSearchForm(forms.Form):
 class ProductSortForm(forms.Form):
     SORT = [
         ("", "Default"),
-        ("price_asc", "Price (low to high)"),
-        ("price_desc", "Price (high to low)"),
         ("name_asc", "Name (A-Z)"),
         ("name_desc", "Name (Z-A)"),
+        ("price_asc", "Price (low to high)"),
+        ("price_desc", "Price (high to low)"),
     ]
 
     sort = forms.ChoiceField(
