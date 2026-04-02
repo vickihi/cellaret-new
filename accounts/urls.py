67390a0681fff580f7c3fbd0302d9f7200102ff3
Weiwei Guo <vickihi2000@gmail.com>
2026-04-02 12:00:56 -0400

    Add login functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,6 @@ app_name = "accounts"
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("signup/submit/", views.signup_submit, name="signup_submit"),
+    path("login/", views.login, name="login"),
+    path("login/submit/", views.login_submit, name="login_submit"),
 ]
